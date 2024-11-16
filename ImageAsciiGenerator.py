481c4d866de35ebd3f34f5aaa6072dfa23e5ef20
M3mberTM <137719297+M3mberTM@users.noreply.github.com>
2024-11-16 23:29:06 +0200

    Adjusted the ImageAsciiGenerator.py to have adjustable font size, and also have 2 slightly different methods for showcasing the final image. Still need to play with the ratio of width to height a bit

--- ImageAsciiGenerator.py
+++ ImageAsciiGenerator.py
@@ -3,77 +3,104 @@ from tkinter.filedialog import askopenfilename
 import numpy as np
 import cv2 as cv
 from PIL import Image, ImageFont, ImageDraw, ImageFilter
+import math
+
 
 #  $@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,"^`'.
 #  .-;+=xX$█
 #  █$Xx=+;-.
+
+# C:\\Users\\risko\\Documents\\Pictures\\cyberpunkGasMask.png
+# ./images/blank_white.png
+
+# TODO better name methods
+# TODO add comments around for better understanding of code later
 class ImageAsciiGenerator:
-    ascii_gradient = " .-;+=xX$"
-    test_file_path = "C:\\Users\\risko\\Documents\\Pictures\\IMG_20200704_141627.jpg"
+    ascii_gradient = " .-;+=xX$█"
+    test_file_path = "./testImages/cyberpunkGasMask.png"  # test image size 1664 x 1664
     TEST = False
     filename = ""
     image_dimensions = None
     text_size = 16
-    width_divisor = 20
-    height_divisor = 14
-    text_font = ImageFont.truetype("cour.ttf", text_size)
+    width_divisor = float
+    height_divisor = float
+    # monospace fonts: consolaz, cour, lucon
+    text_font = ImageFont.truetype("lucon.ttf", text_size)
+    bloom = bool
 
-    def get_index_value(self, brightness: float):
-        divider = 255 / (len(self.ascii_gradient)-1)
+    def get_ascii_index_value(self, brightness: float):
+        divider = 255 / (len(self.ascii_gradient) - 1)
         return int(brightness // divider)
 
-    def __init__(self):
+    def __init__(self, font_size: int, ascii_gradient: str, bloom: bool):
         if not self.TEST:
             Tk().withdraw()  # we don't want a full GUI, so keep the root window from appearing
             self.filename = askopenfilename()
+            self.text_size = font_size
+            self.ascii_gradient = ascii_gradient
+            self.text_font = ImageFont.truetype("lucon.ttf", self.text_size)
         else:
             self.filename = self.test_file_path
+        self.width_divisor = self.text_size / 0.84
+        self.height_divisor = self.width_divisor / 1.5
+        self.bloom = bloom
 
-    def open_image(self) -> list[list]:
+    def load_image(self) -> list[list]:
         img = cv.imread(self.filename, cv.IMREAD_COLOR)
         img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
         self.image_dimensions = np.shape(img)
 
-        new_width = self.image_dimensions[1] // self.width_divisor
-        new_height = self.image_dimensions[0] // self.height_divisor
-
+        new_width = math.floor(self.image_dimensions[1] / self.width_divisor)
+        new_height = math.floor(self.image_dimensions[0] / self.height_divisor)
         resized_img = cv.resize(img, (new_width, new_height))
         return resized_img
 
-    def convert(self, resized_img) -> str:
+    def pixels_to_text(self, resized_img) -> str:
         ascii_img = ""
 
         for i in range(len(resized_img)):
             for j, k in enumerate(resized_img[i]):
-                current_char = self.ascii_gradient[self.get_index_value(k[2])]
+                current_char = self.ascii_gradient[self.get_ascii_index_value(k[2])]
                 ascii_img = ascii_img + current_char + current_char
             ascii_img = ascii_img + "\n"
         return ascii_img
 
-    def draw_on_image(self, text: str, background_color: tuple, text_color: tuple):
+    def draw_text_on_img_adjusted(self, text: str, background_color: tuple, text_color: tuple):
+        width_tolerance = 1.05  # scales the image slightly to take into account the lost accuracy with flooring later
+        height_tolerance = 1.1
+        im = Image.new(mode="RGB", size=(math.floor(self.image_dimensions[1] * width_tolerance), math.floor(self.image_dimensions[0] * height_tolerance)), color=background_color)
+        draw = ImageDraw.Draw(im)
+        text_lines = text.split("\n")
+        # the last line is just an empty string due to the \n character being at the end
+        for i in range(len(text_lines)-2):
+            for j, char in enumerate(text_lines[i]):
+                gradient_length = len(self.ascii_gradient)
+                current_char_position = self.ascii_gradient.index(char)
+                divisor = math.floor((current_char_position / gradient_length)*10) / 10
+                char_color = (math.floor(text_color[0] * divisor), math.floor(text_color[1] * divisor), math.floor(text_color[2] * divisor))
+                x = math.ceil(self.image_dimensions[1] / len(text_lines[i]))
+                y = math.ceil(self.image_dimensions[0] / (len(text_lines) - 1))
+                draw.text((j * x, i * y), char, font=self.text_font, spacing=0, fill=char_color, align="left")
+        return im
+
+    def draw_text_on_image(self, text: str, background_color: tuple, text_color: tuple):
         im = Image.new(mode="RGB", size=(self.image_dimensions[1], self.image_dimensions[0]), color=background_color)
         draw = ImageDraw.Draw(im)
         draw.text((0, 0), text, font=self.text_font, spacing=0, fill=text_color, align ="left")
         return im
 
-    def bloom(self, image, text: str, text_color: tuple):
-        bloomed_image = image
-        bloomed_image.filter(ImageFilter.BoxBlur(4))
-        draw = ImageDraw.Draw(bloomed_image)
-        draw.text((0, 0), text, font=self.text_font, spacing=0, fill=text_color, align ="left")
-        return bloomed_image
 
 if __name__ == "__main__":
 
-    iag = ImageAsciiGenerator()
-    edited_image = iag.open_image()
-    final_text = iag.convert(edited_image)
+    iag = ImageAsciiGenerator(8, " .-;+=xX$", True)
+    edited_image = iag.load_image()
+    final_text = iag.pixels_to_text(edited_image)
     background_color = (22, 3, 43)
-    text_color = (0, 195, 255)
-    im = iag.draw_on_image(final_text, background_color, text_color)
+    foreground_color = (0, 195, 255)
+    im = iag.draw_text_on_img_adjusted(final_text, background_color, foreground_color)
     im.show()
-    if input("Do you want to save? (y/n) ") == 'y':
-        image_name = input("What do you want to name the image?\n")
-        im.save(f'images\{image_name}.png')
-    # bloom_img = iag.bloom(im, final_text, text_color)
-    # bloom_img.show()
+    ask = False
+    if not iag.TEST and ask:
+        if input("Do you want to save? (y/n) ") == 'y':
+            image_name = input("What do you want to name the image?\n")
+            im.save(f'images\{image_name}.png')
--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='Adjusting the image to ascii converter.')
+
+parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                    help='an integer for the accumulator')
+parser.add_argument('--sum', dest='accumulate', action='store_const',
+                    const=sum, default=max,
+                    help='sum the integers (default: find the max)')
+
+args = parser.parse_args()
+print(args.accumulate(args.integers))
+
 # TODO make the arguments for switching between modes. As well as arguments for color in the Image version. Make arguments for adjusting the ascii gradient.
 
-text = "█$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'."
-print(text[::-1])
+
