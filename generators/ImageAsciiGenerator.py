4e080a168b5a35d4859fde0f205af3e7fdeffc34
M3mberTM <137719297+M3mberTM@users.noreply.github.com>
2025-02-27 03:54:46 +0200

    Small issue fix with get_gradient_index function. Used to skip one character, didn't work when gradient was 1 long. Now works

--- generators/ImageAsciiGenerator.py
+++ generators/ImageAsciiGenerator.py
@@ -75,7 +75,7 @@ class ImageAsciiGenerator:
         self.bg_color = background_color
         self.fg_color = foreground_color
         self.effect = effect
-        self.divider = 255 / (len(self.ascii_gradient) - 1)
+        self.divider = 255 / (len(self.ascii_gradient))
 
     def convert(self) -> Image:
         """Main function of the ImageAsciiGenerator Class
@@ -181,7 +181,7 @@ class ImageAsciiGenerator:
 
     def __get_gradient_index(self, brightness: float):
         # limits the range of values to just x values, where x represents the length of the gradient
-        return int(brightness // self.divider)
+        return int(brightness // self.divider) - 1
 
     def __pixels_to_txt(self, resized_img) -> str:
         # uses StringIO to save up on memory
